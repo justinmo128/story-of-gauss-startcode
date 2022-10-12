5c128d45d16c9f0f710d1bcef11781cb89bff098
justinmo128 <j.mo1@share.epsb.ca>
2022-10-12 10:43:55 -0600

    finish

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Script.js
+// Story of Gauss
+let calcBtn = document.getElementById("calc-btn")
+calcBtn.addEventListener("click", calcSum);
+
+let sumOut = document.getElementById("sum-out")
+
+function calcSum() {
+    // Sum integers 1 to 100
+    let n = +document.getElementById("n-in").value;
+    document.getElementById("n-out").innerHTML = n;
+    sumOut.innerHTML = ((n * (n + 1)) / 2);
+}
